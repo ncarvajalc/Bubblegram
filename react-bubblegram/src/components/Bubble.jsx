8f19ed172b113320f45d1fb3404a24178481d7a9
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2022-09-21 22:46:58 -0500

    FIxed warnings and live likes

--- react-bubblegram/src/components/Bubble.jsx
+++ react-bubblegram/src/components/Bubble.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import "../styles/Bubble.css";
 import { Storage } from "aws-amplify";
-import { PostStorage } from '../storage/storage';
+import { PostStorage } from "../storage/storage";
 
 function Bubble({ post }) {
   const [likes, setLikes] = useState(post.likes);
   const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    Storage.get(post.picture_url).then((url) => setImageUrl(url));
-  }, [post]);
+  Storage.get(post.picture_url).then((url) => setImageUrl(url));
 
   function handleLike() {
     PostStorage.likePost(post);
+    setLikes(likes + 1);
   }
 
   function handlePop() {
@@ -24,14 +22,14 @@ function Bubble({ post }) {
   return (
     <div className="bubble">
       <div className="img-background">
-        <img className="bubble-img" src={imageUrl} alt={post.title} />
+        <img className="bubble-img" src={imageUrl} alt="" />
       </div>
       <div className="card-content">
         <Typography variant="body2" color="text.secondary">
           {post.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {post.likes} likes
+          {likes} likes
         </Typography>
       </div>
       <div className="card-actions">
--- react-bubblegram/src/storage/storage.js
+++ react-bubblegram/src/storage/storage.js
@@ -1,77 +1,75 @@
-import { DataStore, Amplify, API } from 'aws-amplify';
-import * as mutations from '../graphql/mutations';
+import { DataStore, Amplify } from "aws-amplify";
 
-import awsconfig from '../aws-exports';
-import { Post, User } from '../models';
-
+import awsconfig from "../aws-exports";
+import { Post, User } from "../models";
 
 Amplify.configure(awsconfig);
 
 export class UserStorage {
-    static async createUserData(newUser) {
-        const userInformation = {
-            user_id: newUser.user_id,
-            email: newUser.email,
-            username: newUser.username,
-            posts: [null]
-        }
-        const userModel = new User(userInformation);
-        await DataStore.save(userModel);
-    }
-    static async loadUserInformation(userId) {
-        const retrievedUser = await DataStore.query(User, userId);
-        return retrievedUser;
-    }
-    static async deleteUserData(userToDelete) {
-        const userModel = await DataStore.query(User, userToDelete.id);   
-        DataStore.delete(userModel);
-    }
-    static async loadAll() {
-        const loadedValue = await DataStore.query(User);
-        console.log(loadedValue);
-    }
+  static async createUserData(newUser) {
+    const userInformation = {
+      user_id: newUser.user_id,
+      email: newUser.email,
+      username: newUser.username,
+      posts: [null],
+    };
+    const userModel = new User(userInformation);
+    await DataStore.save(userModel);
+  }
+  static async loadUserInformation(userId) {
+    const retrievedUser = await DataStore.query(User, userId);
+    return retrievedUser;
+  }
+  static async deleteUserData(userToDelete) {
+    const userModel = await DataStore.query(User, userToDelete.id);
+    DataStore.delete(userModel);
+  }
+  static async loadAll() {
+    const loadedValue = await DataStore.query(User);
+    console.log(loadedValue);
+  }
 }
 export class PostStorage {
-    static async storePost(sender, post) {
-        const newPost = {
-            title: post.title,
-            picture_url: post.picture_url,
-            likes: 0,
-            owner: sender
-        };
-        const newPostModel = new Post(newPost);
-        await DataStore.save(newPostModel);
-        mapPostToUser(sender, newPostModel);
-        async function mapPostToUser(sender, postModel) {
-            const hasNoPost = typeof(sender.posts) == 'undefined';
-            let newPostHistory = sender.posts;
-            if (hasNoPost) {
-                newPostHistory = [];
-            }
-            newPostHistory.push(postModel);
-            await DataStore.save(
-                User.copyOf(sender, updated => {
-                    updated.posts = newPostHistory;
-                })
-            )
-        }
+  static async storePost(sender, post) {
+    const newPost = {
+      title: post.title,
+      picture_url: post.picture_url,
+      likes: 0,
+      owner: sender,
+    };
+    const newPostModel = new Post(newPost);
+    await DataStore.save(newPostModel);
+    mapPostToUser(sender, newPostModel);
+    async function mapPostToUser(sender, postModel) {
+      const hasNoPost = typeof sender.posts == "undefined";
+      let newPostHistory = sender.posts;
+      if (hasNoPost) {
+        newPostHistory = [];
+      }
+      newPostHistory.push(postModel);
+      await DataStore.save(
+        User.copyOf(sender, (updated) => {
+          updated.posts = newPostHistory;
+        })
+      );
     }
-    static async sortPostById() {
-        const getData = await DataStore.query(User);
-        console.log(getData);
-    }
-    static async likePost(post) {
-        const postToLike = await DataStore.query(Post, post.id);
-        const likePost = await DataStore.save(
-            Post.copyOf(postToLike, updated => {
-                updated.likes = postToLike.likes + 1;
-            })
-        );
-    }
-    static async retrieveLikes(post) {
-        const postId = post.id;
-        const postModel = await DataStore.query(Post, postId);
-        const amountOfLikes = postModel.likes;
-        return amountOfLikes;
-    }
+  }
+  static async sortPostById() {
+    const getData = await DataStore.query(User);
+    console.log(getData);
+  }
+  static async likePost(post) {
+    const postToLike = await DataStore.query(Post, post.id);
+    await DataStore.save(
+      Post.copyOf(postToLike, (updated) => {
+        updated.likes = postToLike.likes + 1;
+      })
+    );
+  }
+  static async retrieveLikes(post) {
+    const postId = post.id;
+    const postModel = await DataStore.query(Post, postId);
+    const amountOfLikes = postModel.likes;
+    return amountOfLikes;
+  }
 }
